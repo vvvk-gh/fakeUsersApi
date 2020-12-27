1bbfa6766aa281c8e56770154c5ae39e01f39a6c
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-12-27 19:27:34 +0530

    desktop site completed

--- src/public/assets/js/script.js
+++ src/public/assets/js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(()=>{
+    
     // console.log('Document loaded');
+    function getUsers() {
+        $.get('/api/users' , (users)=>{
+            console.log(users)
+            let tableBody = $("#tableBody");
+            tableBody.empty();
+            for(let user of users){
+                tableBody.append(`
+                <tr>
+                <td>${user.name}</td>
+                <td>${user.age}</td>
+                <td>${user.occupation}</td>
+                <td>${user.email}</td>
+                <td>${user.gender}</td>
+                </tr>`);
+            }
+           
+        })
+    }
+    
+    getUsers();
 
     $('#submit').click(()=>{
         // console.log(`button clicked`);
@@ -10,9 +31,16 @@ $(document).ready(()=>{
     let email = $('#email').val();
     let gender = $("input[name=gender]:checked").val();
     let userData = {email, name , age , occupation , gender} 
-    $.post('/api/users' ,userData,(data)=>{
-        console.log(data);
-    });
+    if(!(age)|| !(name) || !(occupation) || !(email)|| !(gender)){
+        alert('All fields are mandatory');
+    }
+    else{
+        $.post('/api/users' ,userData,(postdata)=>{
+            console.log(postdata);
+            getUsers();
+        });
+    }
+   
 });
     
 
